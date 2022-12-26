7cc6438e79baa3c085fbc9432dab47dc7799f6b5
MartinaDonceva <doncevamartina@yahoo.com>
2022-12-26 16:43:48 +0100

    Navbar class collapse added

--- src/components/CustomHeader.vue
+++ src/components/CustomHeader.vue
@@ -1,15 +1,21 @@
 <template>
-  <nav class="navbar bg-white navbar-expand-lg navbar-light sticky-top">
+  <nav class="navbar navbar-expand-lg navbar-light bg-white">
     <div class="container">
-      <div class="text-center">
-        <p class="text-color mb-0 py-2 display-3">A La Mode</p>
-      </div>
-      <div class="ms-lg-auto">
-        <a class="text-color navbar-brand fs-4 fw-light" href="#">HOME</a>
-        <a class="text-color navbar-brand fs-4 fw-light" href="#">CONTACT</a>
-        <a class="text-color navbar-brand fs-4 fw-light" href="#"
-          ><i class="fa fa-shopping-cart" aria-hidden="true"></i
-        ></a>
+      <p class="text-color mb-0 py-2 display-3 font-fam" href="#">A La Mode</p>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav ms-auto mb-2 mb-lg-0 ">
+          <li class="nav-item">
+            <a class="text-color navbar-brand fs-4 fw-light" href="#">HOME</a></li>
+          <li class="nav-item">
+            <a class="text-color navbar-brand fs-4 fw-light" href="#">CONTACT</a>
+          </li>
+          <li>
+            <a class="text-color navbar-brand fs-4 fw-light" href="#"><i class="fa fa-shopping-cart" aria-hidden="true"></i></a>
+          </li>
+        </ul>
       </div>
     </div>
   </nav>
@@ -22,10 +28,9 @@ export default {
 </script>
 
 <style scoped>
-p {
+.font-fam {
   font-family: "Brush Script MT", cursive;
 }
-
 .text-color {
   color: dimgrey;
 }
