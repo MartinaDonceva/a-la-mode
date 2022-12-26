cdc60c83e0a6c135f9325f4768c828f1cfeefad1
MartinaDonceva <doncevamartina@yahoo.com>
2022-12-26 18:50:14 +0100

    Centered collapse navbar items on medium and smaller devices

--- src/components/CustomHeader.vue
+++ src/components/CustomHeader.vue
@@ -5,8 +5,8 @@
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav ms-auto mb-2 mb-lg-0 ">
+      <div class="collapse navbar-collapse text-center" id="navbarSupportedContent">
+        <ul class="navbar-nav ms-lg-auto mb-2 mb-lg-0">
           <li class="nav-item">
             <a class="text-color navbar-brand fs-4 fw-light" href="#">HOME</a></li>
           <li class="nav-item">
