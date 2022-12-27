5ce44259e37676b282d038432d6e1419b97a619b
MartinaDonceva <doncevamartina@yahoo.com>
2022-12-27 16:53:55 +0100

    Rounded edges on the product and added form-group part

--- src/components/Product.vue
+++ src/components/Product.vue
@@ -4,7 +4,7 @@
       <div class="col-lg-6">
         <img
             src="https://img.global.news.samsung.com/za/wp-content/uploads/2020/12/Holiday-Series-Part-1-Lifestyle-TV_main_1_F.jpg"
-            alt="..." class="w-100">
+            alt="..." class="w-100 rounded-3">
       </div>
       <div class="col-lg-6 text-color mt-5">
         <h1 class="display-3">PRESENT</h1>
@@ -13,8 +13,12 @@
           Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
           when an unknown printer took a galley of type and scrambled it to make a type specimen book.
         </div>
-        <div>Количина:</div>
-        <input type="text" placeholder="1" class="w-25">
+        <div class="form">
+          <div class="form-group">
+            <label for="quantity">Количина:</label>
+            <input type="number" class="form-control w-25 h-25" id="quantity" placeholder="1">
+          </div>
+        </div>
         <button class="btn btn-light w-100 fs-4 mt-5 btn-bg-orange text-white m-auto rounded-0">Додади во кошничка</button>
       </div>
     </div>
